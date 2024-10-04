e081da2514f6d547167009848cf394e12a98956d
nachoogriis <nachogrismartin@gmail.com>
2024-10-04 20:00:41 +0200

    Update upload_data_dvc.sh for message definition

--- upload_data_dvc.sh
+++ upload_data_dvc.sh
@@ -3,7 +3,7 @@
 # This script is used to automate the data uploading phase to DVC
 
 # Get as optional parameter the commit message - Set also default value
-commit_message = "${1:-Track data with DVC}"
+commit_message="${1:-Track data with DVC}"
 
 # Step 1: Add data to local DVC repository
 dvc add data/

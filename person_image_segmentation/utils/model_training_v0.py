845e8b1db779b278591209bdc7ab48d6202f063c
mariarisquesmontalban <mariarisques@gmail.com>
2024-10-05 11:42:32 +0200

    Add CodeCarbon For Model Training

--- person_image_segmentation/utils/model_training_v0.py
+++ person_image_segmentation/utils/model_training_v0.py
@@ -2,27 +2,28 @@ import subprocess
 import sys
 
 # Install necessary packages (uncomment if required)
-subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python", "ultralytics", "mlflow", "dagshub"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python", "ultralytics", "mlflow", "dagshub", "codecarbon"])
 
 import os
 import mlflow
 import dagshub
+from codecarbon import EmissionsTracker
 from ultralytics import YOLO
 
 # Disable W&B logging
 os.environ["WANDB_MODE"] = "offline"
 
 # Set the tracking URI for DagsHub
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_tracking_uri("https://dagshub.com/nachoogriis/TAED2_YOLOs.mlflow")
 
 # Initialize DagsHub integration
 dagshub.init(
-    repo_name="TAED2_YOLOs",
-    repo_owner="nachoogriis",
+   repo_name="TAED2_YOLOs",
+   repo_owner="nachoogriis",
 )
 
 # Set experiment name in MLflow
-mlflow.set_experiment("test-experiment-yolo-v0")
+mlflow.set_experiment("test-experiments")
 
 # Enable MLflow autologging
 mlflow.autolog()
@@ -33,17 +34,12 @@ model = YOLO('yolov8m-seg.pt')
 # Path to the config file in the Kaggle environment
 config_file_path = "/kaggle/input/yolo-training-data/config_yolos.yaml"
 
-# Start a new MLflow run to track the experiment
-with mlflow.start_run(run_name="YoloV8-Training"):
-    
-    # Tuning the model
-    # model.tune(data=config_file_path, epochs=1, imgsz=640, name="Hyps Tuning Second Version", iterations=30)
-    
-    # Training the model
-    results = model.train(data=config_file_path, epochs=1, imgsz=640, name="Yolo Weights")
+# Use CodeCarbon's EmissionsTracker as a context manager
+with EmissionsTracker(gpu_ids=[]) as tracker:
+   # Start a new MLflow run to track the experiment
+   with mlflow.start_run(run_name="YoloV8-Training-Test-CodeCarbon"):
 
-    print("Entrenamiento completado y experimentos registrados en MLflow.")
+       # Training the model
+       results = model.train(data=config_file_path, epochs=1, imgsz=640, name="Yolo Weights")
 
-
-
-
+       print("Entrenamiento completado y experimentos registrados en MLflow.")

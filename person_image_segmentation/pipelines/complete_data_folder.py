1a74d3edd1d0ac57733db5bb5aa9d30ac5d8e3cb
mariarisquesmontalban <mariarisques@gmail.com>
2024-10-22 21:03:16 +0200

    Solve format complete_data_folder file

--- person_image_segmentation/pipelines/complete_data_folder.py
+++ person_image_segmentation/pipelines/complete_data_folder.py
@@ -1,10 +1,15 @@
-import argparse
-import shutil
+"""
+Complete data folder script.
 
+This script completes the data folder by copying specified
+configuration files from the source folder to the destination
+folder. It can be run in test mode to use a different data directory.
+"""
+
+import argparse
 from pathlib import Path
 from person_image_segmentation.config import REPO_PATH, DATA_DIR
-from person_image_segmentation.utils import complete_data_folder
-
+from person_image_segmentation.utils.dataset_utils import complete_data_folder
 
 if __name__ == "__main__":
     # Set up argument parser
@@ -14,13 +19,12 @@ if __name__ == "__main__":
 
     if args.test:
         DATA_DIR = Path(str(DATA_DIR).replace('data', 'test_data'))
-    
+
     config_names = ["config_hyps.yaml", "config_yolos.yaml"]
-    
+
     src_folder = REPO_PATH / "models/configs"
     dst_folder = DATA_DIR
 
-    
     complete_data_folder(
         config_names = config_names,
         src_folder = src_folder,

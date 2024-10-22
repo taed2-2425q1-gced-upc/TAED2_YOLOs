5e9c3d660017d1efe9f891e8a024928c10629b92
mariarisquesmontalban <mariarisques@gmail.com>
2024-10-22 21:03:43 +0200

    Solve format create_labels file

--- person_image_segmentation/pipelines/create_labels.py
+++ person_image_segmentation/pipelines/create_labels.py
@@ -1,3 +1,24 @@
+"""
+Person Image Segmentation Pipeline - Label Generation.
+
+This script generates labels for a person image segmentation dataset. It uses predefined
+directories for transformed data, labels, and split data, and allows for running in test mode
+to use alternative data directories.
+
+Usage:
+    python generate_labels.py [--test]
+
+Args:
+    --test: Run the pipeline in test mode, which changes the data directories to use a
+            test-specific path.
+
+Functionality:
+    - Updates the data directories to point to test directories if the '--test' argument is
+      provided.
+    - Calls the 'generate_labels' function to generate labels for the dataset based on the
+      specified directories.
+"""
+
 import argparse
 from pathlib import Path
 
@@ -15,7 +36,7 @@ if __name__ == "__main__":
         SPLIT_DATA_DIR = Path(str(SPLIT_DATA_DIR).replace('data', 'test_data'))
         TRANSFORM_DATA_DIR = Path(str(TRANSFORM_DATA_DIR).replace('data', 'test_data'))
         LABELS_DATA_DIR = Path(str(LABELS_DATA_DIR).replace('data', 'test_data'))
-    
+
     # Generate labels
     generate_labels(
         transform_dir = TRANSFORM_DATA_DIR,
